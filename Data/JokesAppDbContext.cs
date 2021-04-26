444819b00aeb22155255857a6030fa61d59870a8
vinumahajan <73319505+vinumahajan@users.noreply.github.com>
2021-04-27 00:14:50 +0100

    first commit

---
+++ Data/JokesAppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using JokesApp.Models;
+
+namespace JokesApp.Data
+{
+    public class JokesAppDbContext : DbContext
+    {
+        public JokesAppDbContext (DbContextOptions<JokesAppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<JokesApp.Models.Joke> Joke { get; set; }
+    }
+}
